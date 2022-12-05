6a9bcdcf24dafd7a1635d12600d1a1784953efac
collyrium921 <kajusinghal921@gmail.com>
2022-12-05 16:14:52 +0530

    creating and connecting with database

--- myapp/models.py
+++ myapp/models.py
@@ -2,8 +2,14 @@ import string
 from django.db import models
 
 # Create your models here.
-class Feature:
+class Feature1:
     id:int
     name:str
     details:str
     is_true:bool
+
+class Feature(models.Model):
+    name=models.CharField(max_length=100)
+    details = models.CharField(max_length=500)
+
+
--- myapp/views.py
+++ myapp/views.py
@@ -1,5 +1,7 @@
+from pyexpat import features
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Feature1
 from .models import Feature
 # Create your views here.
 def index1(request):
@@ -20,23 +22,26 @@ def counter(request):
     return render(request, 'counter.html', {'length':length})
 
 def index(request):
-    feature1 = Feature()
-    feature1.id =0
-    feature1.name = 'Fast'
-    feature1.details = 'Our service is very quick'
-    feature1.is_true = True
+    # before creating database
+    # feature1 = Feature1()
+    # feature1.id =0
+    # feature1.name = 'Fast'
+    # feature1.details = 'Our service is very quick'
+    # feature1.is_true = True
     
-    feature2 = Feature()
-    feature2.id =0
-    feature2.name = 'Reliable'
-    feature2.details = 'Our service is very reliable'
-    feature2.is_true = False
+    # feature2 = Feature1()
+    # feature2.id =0
+    # feature2.name = 'Reliable'
+    # feature2.details = 'Our service is very reliable'
+    # feature2.is_true = False
 
-    feature3 = Feature()
-    feature3.id =0
-    feature3.name = 'Easy to use'
-    feature3.details = 'Our service is very easy'
-    feature3.is_true = True
+    # feature3 = Feature1()
+    # feature3.id =0
+    # feature3.name = 'Easy to use'
+    # feature3.details = 'Our service is very easy'
+    # feature3.is_true = True
 
-    features=[feature1, feature2, feature3]
+    # features=[feature1, feature2, feature3]
+
+    features = Feature.objects.all()
     return render(request, 'index.html', {'features':features})   
