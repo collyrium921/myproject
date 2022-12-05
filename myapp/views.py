421576cca737c154f84bde1f4d7938be7ec35e3b
collyrium921 <kajusinghal921@gmail.com>
2022-12-05 15:51:45 +0530

    -models basic - for and if statement in html

--- myapp/views.py
+++ myapp/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Feature
 # Create your views here.
 def index1(request):
     # return HttpResponse('<h1>Hey, Welcome</h1>')
@@ -19,4 +20,23 @@ def counter(request):
     return render(request, 'counter.html', {'length':length})
 
 def index(request):
-    return render(request, 'index.html')   
+    feature1 = Feature()
+    feature1.id =0
+    feature1.name = 'Fast'
+    feature1.details = 'Our service is very quick'
+    feature1.is_true = True
+    
+    feature2 = Feature()
+    feature2.id =0
+    feature2.name = 'Reliable'
+    feature2.details = 'Our service is very reliable'
+    feature2.is_true = False
+
+    feature3 = Feature()
+    feature3.id =0
+    feature3.name = 'Easy to use'
+    feature3.details = 'Our service is very easy'
+    feature3.is_true = True
+
+    features=[feature1, feature2, feature3]
+    return render(request, 'index.html', {'features':features})   
