07e070b0600e3ddbc5961a396298c94a81c4f83c
collyrium921 <kajusinghal921@gmail.com>
2022-12-06 12:49:19 +0530

    -signup -authentication and login -logout

--- myapp/urls.py
+++ myapp/urls.py
@@ -3,5 +3,8 @@ from . import views
 urlpatterns = [
     path('', views.index, name='index'),
     path('index1', views.index1, name='index1'),
-    path('counter', views.counter, name='counter')
+    path('counter', views.counter, name='counter'),
+    path('register', views.register, name='register'),
+    path('login', views.login, name='login'),
+    path('logout', views.logout, name='logout'),
 ]
--- myapp/views.py
+++ myapp/views.py
@@ -1,6 +1,9 @@
 from pyexpat import features
-from django.shortcuts import render
+import re
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
+from django.contrib.auth.models import User, auth
+from django.contrib import messages
 from .models import Feature1
 from .models import Feature
 # Create your views here.
@@ -45,3 +48,48 @@ def index(request):
 
     features = Feature.objects.all()
     return render(request, 'index.html', {'features':features})   
+
+def register(request):
+    if request.method == 'POST':
+        username = request.POST['username']
+        email = request.POST['email']
+        password = request.POST['password']
+        confirmPassword = request.POST['confirmPassword']
+
+        if(password == confirmPassword):
+            if(User.objects.filter(username=username).exists()):
+                messages.info(request, 'Username already exists')
+                return redirect('register')
+            elif(User.objects.filter(email=email).exists()):
+                messages.info(request, 'Email already exists')
+                return redirect('register')
+            else:
+                user = User.objects.create_user(username, email, password)
+                user.is_active = True
+                user.save()
+                return redirect('login')
+        else:
+            messages.info(request, 'Password does not match')
+            return redirect('register')
+    else:
+        return render(request, 'register.html')
+
+def login(request):
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+
+        user = auth.authenticate(username=username, password=password)
+
+        if(user is not None):
+            auth.login(request, user)
+            return redirect('index')
+        else:
+            messages.info(request, 'Invalid credentials')
+            return redirect('login')
+    else:
+        return render(request, 'login.html')
+
+def logout(request):
+    auth.logout(request)
+    return redirect('index')
